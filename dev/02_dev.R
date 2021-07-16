1cae8573f5ddd8853fa739a1e8cfaf2b1639b774
Bruno Caneco <bruno@dmpstats.com>
2021-07-16 18:44:03 +0100

    adding bs4dash

--- dev/02_dev.R
+++ dev/02_dev.R
@@ -17,6 +17,7 @@
 ## Add one line by package you want to add as dependency
 usethis::use_package( "thinkr" )
 usethis::use_package("shinydashboard")
+usethis::use_package("bs4Dash")
 
 ## Add modules ----
 ## Create a module infrastructure in R/
